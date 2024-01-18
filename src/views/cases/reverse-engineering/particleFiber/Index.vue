671ee4927d570cf198a4b640038f5566248b1c87
chenfengyi <chenfengyi@ictnj.ac.cn>
2024-01-18 18:02:01 +0800

    fin: brain shape done

--- src/views/cases/reverse-engineering/particleFiber/Index.vue
+++ src/views/cases/reverse-engineering/particleFiber/Index.vue
@@ -6,7 +6,10 @@ import { onMounted, onUnmounted, ref } from "vue";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { data } from "./assets/data";
-let controls, camera;
+import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
+import brainVertexShader from "./shaders/brain/vertex.glsl?raw";
+import brainFragmentShader from "./shaders/brain/fragment.glsl?raw";
+let controls, camera, elapsedTime;
 const randomRange = (min, max) => Math.random() * (max - min) + min;
 const PATHS = data.economics[0].paths;
 // console.log(PATHS);
@@ -22,45 +25,53 @@ scene.add(new THREE.AmbientLight(0xffffff, 1));
 const pointLight = new THREE.PointLight(0xffffff, 1, 100);
 pointLight.position.set(10, 10, 10);
 scene.add(pointLight);
-// Mesh
-let curves = [];
-// curves
-for (let i = 0; i < 100; i++) {
+
+// brainMesh
+let brainCurves = [];
+PATHS.forEach((path) => {
   let points = [];
-  let length = randomRange(0.1, 1);
-  // points
-  for (let j = 0; j < 100; j++) {
-    points.push(
-      new THREE.Vector3().setFromSphericalCoords(
-        1,
-        Math.PI - (j / 100) * Math.PI * length,
-        (i / 100) * Math.PI * 2
-      )
-    );
+  for (let i = 0; i < path.length; i += 3) {
+    points.push(new THREE.Vector3(path[i], path[i + 1], path[i + 2]));
   }
   let tempcurve = new THREE.CatmullRomCurve3(points);
-  curves.push(tempcurve);
-}
-console.log("wtf");
-//tube
-function Tube() {
-  let points = [];
-
-  for (let i = 0; i < 10; i++) {
-    points.push(new THREE.Vector3((i - 5) * 0.5, Math.sin(i * 2) * 10 + 5, 0));
-  }
-  return new THREE.CatmullRomCurve3(points);
-}
-function Tubes() {
-  curves.map((curve) => {
-    const tubeGeo = new THREE.TubeGeometry(curve, 64, 0.01, 8, false);
-    const material = new THREE.MeshStandardMaterial({ color: "hotpink" });
-    const tube = new THREE.Mesh(tubeGeo, material);
-
+  brainCurves.push(tempcurve);
+});
+// brainMaterial
+const brainMaterial = new THREE.ShaderMaterial({
+  depthWrite: false,
+  side: THREE.DoubleSide,
+  blending: THREE.AdditiveBlending,
+  vertexColors: true, // rgb or rgba
+  transparent: true,
+  depthTest: false,
+  depthWrite: false,
+  uniforms: {
+    // uSize: { value: 30 * renderer.getPixelRatio() },
+    uTime: { value: 0 },
+    uColor: { value: new THREE.Color(0.1, 0.3, 0.6) },
+  },
+  vertexShader: brainVertexShader,
+  fragmentShader: brainFragmentShader,
+  onBeforeCompile: (shader) => {
+    // console.log(shader);
+  },
+});
+function brainTubes() {
+  brainCurves.map((curve) => {
+    const tubeGeo = new THREE.TubeGeometry(curve, 64, 0.001, 3, false);
+    const tube = new THREE.Mesh(tubeGeo, brainMaterial);
     scene.add(tube);
   });
 }
-Tubes();
+brainTubes();
+// brainParticleMesh
+const particleGeometry = new THREE.BufferGeometry();
+const array = new Float32Array(brainCurves.length * 100 * 3);
+const attribute = new THREE.BufferAttribute(array, 3);
+// attribute.count = brainCurves.length * 100
+// console.log(attribute.count)
+particleGeometry.setAttribute("position", attribute);
+
 /**
  * Sizes
  */
@@ -86,8 +97,8 @@ window.addEventListener("resize", () => {
  * Camera
  */
 // Base camera
-camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.set(1, 1, 1);
+camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.001, 5);
+camera.position.set(0, 0, 0.3);
 scene.add(camera);
 
 /**
@@ -97,10 +108,16 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+// Clock
+const clock = new THREE.Clock();
+
 /**
- * Animate
+ * Render
  */
 function render() {
+  elapsedTime = clock.getElapsedTime();
+
+  brainMaterial.uniforms.uTime.value = elapsedTime;
   // Update controls
   controls.update();
 
